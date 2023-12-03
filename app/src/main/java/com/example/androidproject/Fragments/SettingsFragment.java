e8927418d991360da9f01b4fd93092e32f670ee6
w0652557 <w0652557@myscc.ca>
2023-12-02 20:06:06 -0500

    SettingsFragment now works

--- app/src/main/java/com/example/androidproject/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/androidproject/Fragments/SettingsFragment.java
@@ -1,8 +1,17 @@
 package com.example.androidproject.Fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.EditTextPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.example.androidproject.R;
 
@@ -11,5 +20,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        CheckBoxPreference largerTextPreference = findPreference("largeText");
+
+        if (largerTextPreference != null) {
+            largerTextPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                boolean disableAnimations = (boolean) newValue;
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
+                preferences.edit().putBoolean("disableAnimations", disableAnimations).apply();
+                return true;
+            });
+
+        }
     }
 }
